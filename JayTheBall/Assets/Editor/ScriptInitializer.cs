64529ac573b5f89f72cab2beba729558ad85809d
DuanBinbin <binbin.duan@pisofttech.com>
2018-08-28 17:24:57 +0800

    添加Mesh生成和小球反弹脚本

---
+++ JayTheBall/Assets/Editor/ScriptInitializer.cs
@@ -0,0 +1,28 @@
+﻿/*
+* ==============================
+* FileName:		ScriptInitializer
+* Author:		DuanBin
+* CreateTime:	8/6/2018 4:36:16 PM
+* Description:	用于初始化脚本文件，使用前请先修改Unity的脚本模板，该文件放置于Editor文件夹下	
+* ==============================
+*/
+using System.IO;
+
+public class ScriptInitializer : UnityEditor.AssetModificationProcessor {
+
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.ToLower().EndsWith(".cs") || path.ToLower().EndsWith(".lua"))
+        {
+            string content = File.ReadAllText(path);
+
+            content = content.Replace("#AUTHORNAME#", "DuanBin");
+            content = content.Replace("#CREATETIME#", System.DateTime.Now.ToString());
+
+            File.WriteAllText(path, content);
+        }
+    }    
+}
+
+
